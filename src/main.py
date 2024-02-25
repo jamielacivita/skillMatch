374c8fae06bd99ada7a8fd81355b0f8129e112f9
jamie <JCLacivita@gmail.com>
2024-02-25 09:02:20 -0700

    Output latest CSV file.

--- src/main.py
+++ src/main.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
 
     match_set_object = MatchSet.MatchSet()
 
-    match_set_object.print_match_chart()
+    #match_set_object.print_match_chart()
 
 
     print(f"Number of externs : {match_set_object.get_number_externs()}")
@@ -38,7 +38,7 @@ if __name__ == "__main__":
     print(f"Number of matches : {match_set_object.get_number_matches()}")
 
     #match_set_object.print_extern_city()
-    #match_set_object.save_match_chart_csv()
+    match_set_object.save_match_chart_csv()
 
 
 
