43173efc28d640faefdb26ba3d8106497f090f77
jamie <JCLacivita@gmail.com>
2025-03-12 18:58:28 -0600

    After updating zipcode dictionary.

--- src/data_config.py
+++ src/data_config.py
@@ -4,7 +4,7 @@ LoggingConfigurationFile = r"/home/jamie/source/skillMatch/src/logging_config.in
 # This is the path to the csv file listing the educators who have applied to teach int the program.
 EdApplicationPath = r"/home/jamie/source/skillMatch/data/Input/250311_Externship_ExternApplication.csv"
 
-# This is the path to the xlsx file listing the sites who want to host a teacher to teach in the program.
-HostApplicationPath = r"/home/jamie/source/skillMatch/data/Input/250311_HostApplication.csv"
+# This is the path to the csv file listing the sites who want to host a teacher to teach in the program.
+HostApplicationPath = r"/home/jamie/source/skillMatch/data/Input/250311_Externship_HostApplication.csv"
 
 distance_miles_pickle = r"/home/jamie/source/skillMatch/data/Input/distance_miles.pickle"
